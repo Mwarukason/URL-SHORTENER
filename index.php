80799ac3012bebc6f446c5a31c6ce5913cc95da0
Mwarukason <a.mwaruka93@gmail.com>
2017-05-21 04:39:54 +0800

    creating index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>URL Shortner</title>
+     <style type = "text/css">
+       body{
+         margin: 0;
+         padding: 0;
+       }
+     </style>
+   </head>
+   <body>
+     <form class="" action="index.html" method="post">
+       <input type="text" name="url" value="Enter your url..">
+       <input type="submit" name="" value="">
+     </form>
+
+   </body>
+ </html>
