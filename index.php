0f6b32a03121288da40c9a1a9382f8fe38dfe2f6
Mwarukason <a.mwaruka93@gmail.com>
2017-05-21 16:29:48 +0800

    added style for html

--- index.php
+++ index.php
@@ -15,28 +15,90 @@
     file_put_contents('urls.json',json_encode($urls));
     //user doesnt know Shortened url, so need to print
     //to see, whenever user reg new url will see.
-    echo "Shortened URL: localhost/url-shortene/url.php?url={$random}";
+    echo "Shortened URL: localhost/url-shortener/url.php?url={$random}";
   }
 
- ?>
+?>
 
- <!DOCTYPE html>
- <html>
-   <head>
-     <meta charset="utf-8">
-     <title>URL Shortner</title>
-     <style type = "text/css">
-       body{
-         margin: 0;
-         padding: 0;
-       }
-     </style>
-   </head>
-   <body>
-     <form class="" action="" method="POST">
-       <input type="text" name="url" value="Enter your url..">
-       <input type="submit" name="" value="Submit URL ">
-     </form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome To URL Shortener Website</title>
+    <style type = "text/css">
+        body{
+          margin: 0;
+          }
+        .form{
+          margin: 0 auto;;
+          display: flex;
+          justify-content: center;
+        }
 
-   </body>
- </html>
+        /* Add padding to formContent elements */
+        .formContent {
+            width: 50%;
+            margin: 0;
+            padding: 0;
+        }
+        .formContent h3{
+          text-align: center;
+          color: white;
+        }
+
+        /* Full-width inputBtn fields */
+        .inputBtn {
+            width: 60%;
+            padding: 10px;
+            margin: 10px;
+            border-radius: 4px;
+            display: inline-block;
+            font-family: Helvetica;
+            border: 1px solid #ccc;
+            float: left;
+            border: 1px solid #e6e6e6;
+
+        }
+
+        .label{
+          width: 30%;
+          float: left;
+          padding: 10px;
+          font-family: Helvetica;
+        }
+        /* Set a style for register submit buttons */
+        .submitBtn {
+            width: 98%;
+            color: white;
+            border: none;
+            height: 50px;
+            cursor: pointer;
+            font-size: 18px;
+            line-height: 50px;
+            border-radius: 5px;
+            text-align: center;
+            font-family: Helvetica;
+            background-color: #293ca6;
+
+            /*shadow*/
+            -moz-box-shadow: 0 4px 0px rgba(0, 0, 0, 0.4);
+             -webkit-box-shadow: 0 4px 0px rgba(0, 0, 0, 0.4);
+             box-shadow: 0 4px 0px rgba(0, 0, 0, 0.4);
+        }
+    </style>
+  </head>
+  <body>
+    <form method="POST" class="form" action="" name="regForm">
+    <div class="formContent">
+    <h3 class="submitBtn">WELCOME TO URL SHORTEN SITE</h3>
+    <div>
+      <label class="label">PLEASE, ENTER YOUR URL HERE:</label>
+      <input class="inputBtn" name="url" type="text" placeholder="Enter url here." name="un" />
+      <br>
+    </div>
+    <input type="submit" value="ENTER" class="submitBtn" />
+    </div>
+  </div>
+</form>
+  </body>
+</html>
